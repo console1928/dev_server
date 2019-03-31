85a828b96da346ba6f62353441a0a88b7d13b335
Admin <riverflow1928@yandex.ru>
2019-03-31 16:02:42 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const http = require("http");
+
+const server = http.createServer(
+    function (request, response) {
+        console.log(request.method, request.url);
+        if (request.url === "/style.css") {
+            const style = fs.readFileSync("style.css", "utf8");
+            response.end(style);
+        } else {
+            const index = fs.readFileSync("index.html", "utf8");
+            response.end(index);
+        }
+    }
+);
+
+server.listen(3000);
+console.log("server started");
